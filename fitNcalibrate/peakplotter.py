42071a7efa1606986f15504a574a25d34142bd49
Claudio Barros <71285780+cebarros@users.noreply.github.com>
2022-01-12 16:29:13 -0500

    Update peakplotter.py

--- fitNcalibrate/peakplotter.py
+++ fitNcalibrate/peakplotter.py
@@ -7,10 +7,11 @@ import tdrstyle
 # Add a zero to the theoretical values to make a nice y=x plot
 theor=array('d',[0.0,65.740336799410,67.570939637681,69.390239814814])
 #exper=array('d',[64.3,66.05,66.97]) # Pseudo
-exper=array('d',[64.89,66.34,67.09])
+exper=array('d',[64.56,65.4,66.52])
 theor_err=array('d',[0.0079,0.0077,0.0076])
 #exper_err=array('d',[0.14,0.14,0.135]) # Pseudo
-exper_err=array('d',[0.59,0.26,0.64])
+exper_err=array('d',[0.2591,0.2538,0.2467])
+
 
 #for i in range(0,len(exper)):
 #    exper[i]=(exper[i]-16.51)/0.729
@@ -26,7 +27,16 @@ graph.SetMarkerStyle(21)
 graph.SetTitle("")
 graph.GetXaxis().SetTitle("Predicted Energy Peak Position [GeV]")
 graph.GetYaxis().SetTitle("Measured Energy Peak Position [GeV]")
-graph.GetYaxis().SetTitleOffset(1.4)
+graph.GetXaxis().SetTitleSize(0.04)
+graph.GetYaxis().SetTitleSize(0.04)
+graph.GetXaxis().SetTitleOffset(1.2)
+graph.GetYaxis().SetTitleOffset(1.3)
+
+graph.GetXaxis().SetLimits(63,70)
+#graph.GetYaxis().SetRangeUser(63,70)
+graph.SetMaximum(70)
+graph.SetMaximum(63)
+
 
 gfit = TF1("f","[0] + [1] * x")
 graph.Fit(gfit)
@@ -60,7 +70,7 @@ label2.SetTextSize(0.06)
 label2.SetTextAlign(11)
 label2.DrawLatex(0.33, 0.92, "#it{Simulation}")
 
-leg = TLegend(0.1,0.78,0.38,0.9)
+leg = TLegend(0.13,0.75,0.41,0.87)
 leg.AddEntry(graph,"Simulation","p")
 leg.AddEntry(ideal,"Expectation","l")
 leg.AddEntry(gfit,"Fit","l")
